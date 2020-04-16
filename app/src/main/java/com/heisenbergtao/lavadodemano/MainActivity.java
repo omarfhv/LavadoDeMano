6a3b64ead7077acd1fb4f0c3ddf3ed2f41fb1bf8
omarfhv <34494215+omarfhv@users.noreply.github.com>
2020-04-15 21:00:48 -0500

    ya casi queda

--- app/src/main/java/com/heisenbergtao/lavadodemano/MainActivity.java
+++ app/src/main/java/com/heisenbergtao/lavadodemano/MainActivity.java
@@ -2,14 +2,21 @@ package com.heisenbergtao.lavadodemano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayout botonselena, botonramito, botoncumbia,botonqueen, botonjuanga,botontusa,botonluismi,botonchona;
+    LinearLayout botonselena, botonramito, botoncumbia,botonqueen, botonjuanga,botontusa,botonluismi,botonchona,botonrams;
+    ColorDrawable dialogColor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         botonramito = findViewById(R.id.btnramito);
         botonramito.setOnClickListener(this);
+
+        botonrams = findViewById(R.id.btnqramst);
+        botonrams.setOnClickListener(this);
 
         botoncumbia = findViewById(R.id.btncumbia);
         botoncumbia.setOnClickListener(this);
@@ -112,7 +122,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
 
                 break;
+
+            case R.id.btnqramst:
+                Intent intent117 = new Intent(this, CancionDaHast.class);
+                startActivity(intent117);
+                finish();
+
+                break;
         }
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+
+            View vi = inflater.inflate(R.layout.dialogconfirm, null);
+            builder.setView(vi);
+
+
+            final AlertDialog dialog = builder.create();
+            dialog.getWindow().setBackgroundDrawable(dialogColor);
+
+            //decidir despues si sera cancelable o no
+            dialog.setCancelable(false);
+            Button botonsi = vi.findViewById(R.id.botonsi);
+            botonsi.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+                            MainActivity.super.onDestroy();
+                            System.exit(0);
+                        }
+                    }
+            );
+            Button botonno = vi.findViewById(R.id.botonno);
+            botonno.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+
+                        }
+                    }
+            );
+            dialog.show();
+            //Metodos.dialogo( this, getLayoutInflater(), "¿seguro deseas salir de la aplicacion?", 0 );
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
